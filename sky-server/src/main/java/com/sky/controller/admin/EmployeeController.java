831003c328e94a6583996728a96bf3d19220a487
qinhan <3091533438@qq.com>
2025-07-28 11:03:40 +0800

    feat(employee): 增加员工信息查询和编辑功能
    - 在 EmployeeController 中添加了根据 ID 查询员工信息和修改员工信息的接口
    - 在 EmployeeMapper 中添加了根据 ID 查询员工信息的 SQL语句
    - 在 EmployeeService接口中定义了查询和修改员工信息的方法
    - 在 EmployeeServiceImpl 中实现了查询和修改员工信息的逻辑
    - 优化了 EmployeeMapper.xml 中的 SQL 语句，将 id_Number 字段名改为 id_number

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -111,4 +111,26 @@ public class EmployeeController {
         return Result.success();
     }
 
+    /**
+     * 根据id查询员工
+     */
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询员工信息")
+    public Result<Employee> getById(@PathVariable Long id){
+        log.info("根据id查询员工,{}",id);
+        return Result.success(employeeService.getById(id));
+    }
+
+    /**
+     * 编辑员工信息
+     * @param employeeDTO
+     * @return
+     */
+    @PutMapping
+    @ApiOperation("修改员工信息")
+    public Result update(@RequestBody EmployeeDTO employeeDTO){
+        log.info("编辑员工信息{}",employeeDTO);
+        employeeService.update(employeeDTO);
+        return Result.success();
+    }
 }
