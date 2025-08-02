bfa0dc694acbda397be636d3c595bf7f1766f162
qinhan <3091533438@qq.com>
2025-08-02 15:38:10 +0800

    feat(dish): 添加菜品分页查询功能
    - 在 DishController 中添加菜品分页查询的 API接口
    - 在 DishService接口中定义菜品分页查询的方法
    - 实现 DishServiceImpl 中的菜品分页查询逻辑
    - 在 DishMapper 中添加菜品分页查询的 SQL 接口
    - 在 DishMapper.xml 中编写菜品分页查询的 SQL 语句

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,10 +1,15 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface DishMapper {
@@ -23,4 +28,11 @@ public interface DishMapper {
      */
     @AutoFill(OperationType.INSERT)
     void insert(Dish dish);
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
