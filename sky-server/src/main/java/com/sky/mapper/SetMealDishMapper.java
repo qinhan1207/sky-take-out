681211922bc2a85566cba29dc5fcbb59572893b6
qinhan <3091533438@qq.com>
2025-08-04 09:12:37 +0800

    feat(dish): 添加菜品批量删除功能
    - 在 DishController 中添加批量删除接口
    - 在 DishService 中实现批量删除逻辑
    - 在 DishMapper 和 DishFlavorMapper 中添加批量删除相关方法
    - 新增 SetMealDishMapper 用于检查菜品是否被套餐关联
    - 优化了菜品删除的判断逻辑，确保只删除符合条件的菜品

---
+++ sky-server/src/main/java/com/sky/mapper/SetMealDishMapper.java
@@ -0,0 +1,17 @@
+package com.sky.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface SetMealDishMapper {
+
+    /**
+     * 根据菜品id来查询对应的套餐id
+     * @param dishIds
+     * @return
+     */
+    List<Long> getSetMealIdsByDishIds(List<Long> dishIds);
+}
