4b6566f15125de4662e198eb57bd1bd10fc44fcc
qinhan <3091533438@qq.com>
2025-08-06 21:38:25 +0800

    feat(user): 添加用户端查询分类接口
    - 新增 CategoryController 类，提供用户端查询分类的接口
    - 修改 CategoryMapper 接口，增加 selectByType 方法
    - 更新 CategoryMapper.xml，添加 selectByType 方法的 SQL 语句

--- sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -50,6 +50,5 @@ public interface CategoryMapper {
      * @param type
      * @return
      */
-    @Select("select * from category where type = #{type}")
     List<Category> selectByType(Integer type);
 }
