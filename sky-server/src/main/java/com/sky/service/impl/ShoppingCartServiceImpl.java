3dc9e8ca4cbf4b9f68d37d991fe845ccc3c8c232
qinhan <3091533438@qq.com>
2025-08-08 21:36:43 +0800

    feat(user): 添加查看购物车功能
    - 在 ShoppingCartController 中添加 /list 接口，用于查看购物车内容
    - 在 ShoppingCartService接口中添加 showShoppingCart 方法- 在 ShoppingCartServiceImpl 中实现 showShoppingCart 方法，根据当前用户 ID 查询购物车列表

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -81,4 +81,20 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         }
 
     }
+
+    /**
+     * 查看购物车
+     *
+     * @return
+     */
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        // 获取当前微信用户的id
+        Long currentId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                .userId(currentId)
+                .build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        return list;
+    }
 }
