a8d5beb09cd5b0d48fa1190cba1d69981ee2bc40
qinhan1207 <3091533438@qq.com>
2025-09-03 15:49:19 +0800

    feat(order): 添加接单功能- 在 OrderController 中添加 confirm 方法处理接单请求 - 在 OrderService 接口中定义 confirm 方法- 在 OrderServiceImpl 中实现 confirm 方法，更新订单状态为已接单

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 
+import com.sky.dto.OrdersConfirmDTO;
 import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
@@ -11,10 +12,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController("adminOrderController")
 @RequestMapping("/admin/order")
@@ -27,19 +25,20 @@ public class OrderController {
 
     @GetMapping("/conditionSearch")
     @ApiOperation("订单的条件查询")
-    public Result<PageResult> page(OrdersPageQueryDTO ordersPageQueryDTO){
-        log.info("订单条件查询:{}",ordersPageQueryDTO);
+    public Result<PageResult> page(OrdersPageQueryDTO ordersPageQueryDTO) {
+        log.info("订单条件查询:{}", ordersPageQueryDTO);
         PageResult pageResult = orderService.page(ordersPageQueryDTO);
         return Result.success(pageResult);
     }
 
     /**
      * 各个状态的订单数量统计
+     *
      * @return
      */
     @GetMapping("/statistics")
     @ApiOperation("各个状态的订单数量统计")
-    public Result<OrderStatisticsVO> statistic(){
+    public Result<OrderStatisticsVO> statistic() {
         log.info("各个状态的订单数量统计");
         OrderStatisticsVO orderStatisticsVO = orderService.statistic();
         return Result.success(orderStatisticsVO);
@@ -47,9 +46,22 @@ public class OrderController {
 
     @GetMapping("/details/{id}")
     @ApiOperation("查询订单详情")
-    public Result<OrderVO> details(@PathVariable Long id){
-        log.info("查询订单详情：{}",id);
+    public Result<OrderVO> details(@PathVariable Long id) {
+        log.info("查询订单详情：{}", id);
         OrderVO detail = orderService.getDetail(id);
         return Result.success(detail);
+    }
+
+    /**
+     * 接单
+     * @param ordersConfirmDTO
+     * @return
+     */
+    @PutMapping("/confirm")
+    @ApiOperation("接单")
+    public Result confirm(@RequestBody OrdersConfirmDTO ordersConfirmDTO) {
+        log.info("接单：{}", ordersConfirmDTO);
+        orderService.confirm(ordersConfirmDTO);
+        return Result.success();
     }
 }
--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.sky.service;
 
+import com.sky.dto.OrdersConfirmDTO;
 import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
@@ -69,4 +70,10 @@ public interface OrderService {
      * @return
      */
     OrderStatisticsVO statistic();
+
+    /**
+     * 接单
+     * @param ordersConfirmDTO
+     */
+    void confirm(OrdersConfirmDTO ordersConfirmDTO);
 }
