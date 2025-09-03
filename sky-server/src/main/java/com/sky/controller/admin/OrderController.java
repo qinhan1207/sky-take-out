45a9c485e8deae342df272cb8eaceb05ff885948
qinhan1207 <3091533438@qq.com>
2025-09-03 15:31:31 +0800

    feat(sky-server): 添加管理端查询订单详情功能
    - 在 OrderController 中新增 details 方法，用于查询指定订单的详细信息
    - 添加了 OrderVO 类用于封装订单详情数据- 在前端页面中集成查询订单详情的功能

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -6,11 +6,13 @@ import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderStatisticsVO;
+import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,5 +43,13 @@ public class OrderController {
         log.info("各个状态的订单数量统计");
         OrderStatisticsVO orderStatisticsVO = orderService.statistic();
         return Result.success(orderStatisticsVO);
+    }
+
+    @GetMapping("/details/{id}")
+    @ApiOperation("查询订单详情")
+    public Result<OrderVO> details(@PathVariable Long id){
+        log.info("查询订单详情：{}",id);
+        OrderVO detail = orderService.getDetail(id);
+        return Result.success(detail);
     }
 }
